1515492979d59bd05697722db86991da1f7732f7
Tim <timothy.w.holman@gmail.com>
2014-06-17 13:32:15 -0400

    initial commit

---
+++ js/intense-image-viewer.js
@@ -0,0 +1,18 @@
+/*!
+ * intense-image-viewer
+ *
+ * MIT licensed
+ * Copyright (C) 2013 Tim Holman, http://tholman.com
+ */
+
+/*********************************************
+ *
+ *********************************************/
+
+function app() {
+
+    this.init = function() {
+
+    }
+
+}
