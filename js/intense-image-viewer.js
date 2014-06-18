0b0fc9632b92d2be5c63314ceb2eea37c1d17d05
Tim <timothy.w.holman@gmail.com>
2014-06-18 18:25:35 -0400

    scaffolding

--- js/intense-image-viewer.js
+++ js/intense-image-viewer.js
@@ -1,18 +1,128 @@
-/*!
- * intense-image-viewer
- *
- * MIT licensed
- * Copyright (C) 2013 Tim Holman, http://tholman.com
- */
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
 
-/*********************************************
- *
- *********************************************/
+window.cancelRequestAnimFrame = ( function() {
+    return window.cancelAnimationFrame          ||
+        window.webkitCancelRequestAnimationFrame    ||
+        window.mozCancelRequestAnimationFrame       ||
+        window.oCancelRequestAnimationFrame     ||
+        window.msCancelRequestAnimationFrame        ||
+        clearTimeout
+} )();
 
-function app() {
 
-    this.init = function() {
+// Using GSAPS Tween light
+var imageViewer = (function() {
 
+    'use strict';
+    // TODO: Extend module to allow horizontal and vertical 
+    var mouse = { x:0, y:0 };
+
+    // Variable to handle mouse position  
+    var looper;
+  
+    // Current position of scrolly element
+    var currentPosition = 0;
+    
+    var target;
+    var targetDimensions = { w: 0, h: 0 };
+  
+    var container;
+    var containerDimensions = { w: 0, h:0 };
+  
+    var overflowArea = { x: 0, y: 0 };
+
+    
+    // Soft object augmentation
+    function extend( target, source ) {
+
+        for ( var key in source )
+
+            if ( !( key in target ) )
+
+                target[ key ] = source[ key ];
+
+        return target;
+    }
+  
+    function start() { 
+      setDimensions();
+      loop();
+    }
+ 
+    function loop() {
+        looper = requestAnimFrame(loop);
+        positionTarget();      
+    }
+  
+    function stop() {
+      cancelRequestAnimFrame( looper );
     }
 
-}
+    function createViewer() {
+
+
+      targetDimensions = { w: target.width, h: target.height };
+      containerDimensions = { w: container.innerWidth, h: container.innerHeight };
+      overflowArea = {x: containerDimensions.w - targetDimensions.w, y: containerDimensions.h - targetDimensions.h};
+    }
+
+    function startTracking( targetElement, containerElement ) {
+      
+       target = targetElement;
+       container = containerElement;
+       
+       // Bind mouse move
+       // bindMouseMove();
+
+       createViewer();
+       loop();
+      
+      return this;
+    }
+  
+    function onMouseMove( event ) {
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+    }
+  
+    function positionTarget() {
+      currentPosition += (mouse.y - currentPosition);
+      var position = parseFloat(currentPosition / containerDimensions.h );
+      position = Math.ceil(overflowArea.y * position) + "px";
+      // TweenLite.to( target, 0.9, {
+      //     y: position,
+      //     ease: false,
+      //     force3D: true
+      // })
+    }
+
+    function main( target, container ) {
+
+        // Parse arguments
+
+        if ( !target || !container ) {
+
+            throw 'You must supply a target and a container';
+        }
+      
+        // Do it
+        var scrollSystem = mouseScroll( target, container );
+
+        return scrollSystem;
+    }
+
+    return extend( main, {
+        start: start,
+        stop: stop,
+        resize: setDimensions
+    });
+
+})();
+
